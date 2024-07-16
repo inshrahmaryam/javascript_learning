084668440c16edf9adfcc11416811a3eb52f3b82
Inshrah Maryam <inshrahmaryam68@gmail.com>
2024-07-16 16:46:01 +0500

    scope of variables

--- code.js
+++ code.js
@@ -1,1 +1,20 @@
 console.log("hello this is maryam");
+console.log("this is our first js code");
+name="Inshrah Maryam";
+console.log(name);
+price="32.4";
+console.log(price);
+// age="20";
+// console.log(age);
+isFollow=false;
+console.log(isFollow); //boolean
+price="twenty seven rupees";
+console.log(price);
+let fullName="azam shah";
+console.log(fullName);
+const age="20";
+console.log(age);
+{
+    let a=5;
+    console.log(a);
+}
