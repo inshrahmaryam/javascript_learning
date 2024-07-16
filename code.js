704c4bbf7a7e73be85ab1cccd0f217d5092f39d7
Inshrah Maryam <inshrahmaryam68@gmail.com>
2024-07-16 16:20:34 +0500

    variables and boolean in js

--- code.js
+++ code.js
@@ -1,1 +1,12 @@
 console.log("hello this is maryam");
+console.log("this is our first js code");
+name="Inshrah Maryam";
+console.log(name);
+price="32.4";
+console.log(price);
+age="20";
+console.log(age);
+isFollow=false;
+console.log(isFollow); //boolean
+price="twenty seven rupees";
+console.log(price);
