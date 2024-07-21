94a1a7f19104aa088c96eabd873bbc0b9c2d1781
Inshrah Maryam <inshrahmaryam68@gmail.com>
2024-07-21 18:56:43 +0500

    operators in js

--- code.js
+++ code.js
@@ -34,3 +34,40 @@ const profile={
     following:4,
 };
 console.log(typeof profile["followers"]);
+//arithmetic operators
+let a=8; 
+let b=3;
+console.log("a+b=", a +b);
+console.log("a*b=",a*b);
+console.log("a/b=",a/b);
+console.log("a%b=",a%b);
+console.log("a**b=",a**b); //exponenciation:raised to power
+//unary poerator
+a++;
+console.log(a);
+--b;
+// console.log(--b);
+console.log(b--);
+console.log(b);
+//assignment operators
+let z=5;
+let y=2;
+z+=6;
+console.log("z+=6=",z);
+y*=2;
+console.log("y*2=",y);
+//comparison operators : returns boolean value
+console.log("5==2",z==y);
+console.log("5!=2",z!=y);
+console.log("5===2",5===2); //also compares datatypes
+console.log("5!==2", z!==y);
+console.log("5<=2", z<=y);
+console.log("2<=5",y<=z);
+//logical operators
+let o=1;
+let p=2;
+let cond1=o<p;
+let cond2=p===9; 
+console.log("cond1&&cond2=",cond1&&cond2); 
+console.log("cond1||cond1=", cond1||cond2);
+console.log(!o>p);
