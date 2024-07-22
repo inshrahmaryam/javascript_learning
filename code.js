726ea0e51d61958c01a52d8e80323dbdf69a55fb
Inshrah Maryam <inshrahmaryam68@gmail.com>
2024-07-22 16:38:36 +0500

    while, do while, for in, for of loops in js

--- code.js
+++ code.js
@@ -119,5 +119,37 @@ for(let i=1;i<=5;i++){
     sum=sum+i;
     console.log("sum=",sum);
 }
+let k=0;
+while(k<=10){
+    console.log("k=",k);
+    k++;
+}
+let r=1;
+do{
+    console.log("hello i am coding in js");
+    r++;
+} while(r<=20);
+let str="maryam";
+for(let l of str){
+    console.log(l);
+}
+let teacher={
+    name:"madiha memon",
+    age:30,
+    designation:"associate professor",
+};
+for(let y in teacher){
+    console.log(y);
+    console.log("key=",y,"value",teacher[y]);
+} //returns keys
+let gameNum=25;
+let userNum=prompt("guess the game number");
+while(userNum!=gameNum){
+    console.log("oops! u guessed the wrong no. now guess again");
+}
+    console.log("congrats! u entered the right number");
+
+
+
 
 
